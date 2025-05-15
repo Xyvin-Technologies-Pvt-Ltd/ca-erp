273f58f2dccf0db2c501864d46b7585f61e56b0a
xyvintech <admin@xyvin.com>
2025-05-15 19:47:26 +0530

    test

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api-ca-erp.xyvin.com/api/';
 
 export const ROLES = {
     ADMIN: 'admin',
