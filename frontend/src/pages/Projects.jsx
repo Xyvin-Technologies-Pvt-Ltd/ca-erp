ce88c0c97fe46e55d7a49da5c302ef1de3785dd5
fayiza <fayizamp3@gmail.com>
2025-06-28 17:52:15 +0530

    project icon

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -345,18 +345,18 @@ const Projects = () => {
                           <span className="text-xs font-medium text-gray-600">
                             {/* {member.charAt(0)} */}
                              <svg
-                  className="h-5 w-5 text-gray-600"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
+                                className="h-5 w-5 text-gray-400"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                                />
+                              </svg>
                           </span>
                         </div>
                       ))}
