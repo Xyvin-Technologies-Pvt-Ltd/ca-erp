297f0acea219474b0767d214335d9a4941a98923
fayiza <fayizamp3@gmail.com>
2025-08-28 17:54:45 +0530

    background converted to blur in project and task

--- frontend/src/components/CreateProjectModal.jsx
+++ frontend/src/components/CreateProjectModal.jsx
@@ -27,7 +27,7 @@ const CreateProjectModal = ({ isOpen, onClose, onProjectCreated }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0  bg-opacity-50 backdrop-blur-sm z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 bg-black/20 backdrop-blur-sm z-50 flex items-center justify-center">
       <div
         ref={modalRef}
         className="max-w-4xl w-full mx-4 max-h-[90vh] overflow-y-auto"
