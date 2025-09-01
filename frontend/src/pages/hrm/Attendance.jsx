0d00b709c510468d14d1c404140c1fbab4ac6b6a
fayiza <fayizamp3@gmail.com>
2025-09-01 10:50:25 +0530

    bulk attendance ui adjusted

--- frontend/src/pages/hrm/Attendance.jsx
+++ frontend/src/pages/hrm/Attendance.jsx
@@ -251,7 +251,7 @@ const Attendance = () => {
             <CalendarIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
           </div>
           <motion.button
-            className="group px-6 py-3 bg-[#1c6ead] text-white rounded-xl hover:from-blue-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-[#1c6ead] focus:ring-offset-2 transition-all duration-200 cursor-pointer font-semibold shadow-lg hover:shadow-xl flex items-center"
+            className="group px-5 py-2 bg-[#1c6ead] text-white rounded-lg hover:from-blue-700 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-[#1c6ead] focus:ring-offset-2 transition-all duration-200 cursor-pointer font-semibold shadow-lg hover:shadow-xl flex items-center"
             onClick={() => setModalOpen(true)}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
