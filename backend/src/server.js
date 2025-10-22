5064817c41f0829a3863ebaf17a827b3798b3f5b
favasvp <favasvp150@gmail.com>
2025-10-22 12:17:10 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -76,7 +76,7 @@ cronService.init();
 
 // CORS configuration
 const corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://api-ca-erp.xyvin.com', 'https://ca-erp.xyvin.com'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://api-ca-erp.xyvin.com', 'https://ca-erp.xyvin.com' , 'https://testerp.xyvin.com' , 'https://api-testerp.xyvin.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
